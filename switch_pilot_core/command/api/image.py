e9fdc35f97866b01f8b04d4faee0654db35aadef
carimatics <github.carimatics@gmail.com>
2023-12-08 22:53:22 +0900

    Add easyocr support in image.py
    The implemented static method in image.py creates an easyocr.Reader object for text detection. This method provides modularity in the text detection process. Furthermore, the changes allow optional Reader parameters in the detect_text and contains_text methods, promoting customizability and efficiency.

--- switch_pilot_core/command/api/image.py
+++ switch_pilot_core/command/api/image.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from easyocr import easyocr
+
 from switch_pilot_core.image import Image, ImageRegion
 from switch_pilot_core.path import Path
 
@@ -10,6 +14,10 @@ class CommandImageAPI:
     def read_template(self, name: str, use_gray_scale: bool = True) -> Image:
         path = self._path.template(command=self._command, name=name)
         return Image.from_file(file_path=path, use_gray_scale=use_gray_scale)
+
+    @staticmethod
+    def create_text_reader(langs: Optional[list[str]] = None) -> easyocr.Reader:
+        return Image.get_text_reader(langs=langs)
 
     @staticmethod
     def create_region(x: tuple[float, float], y: tuple[float, float]) -> ImageRegion:
